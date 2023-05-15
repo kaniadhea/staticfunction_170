458f2f883fa037b6361a98acc1f4357b3a5c970a
kaniadhea <kaniadhea7@gmail.com>
2023-05-15 01:33:33 -0700

    void mahasiswa

--- staticfunction_170/staticfunction_170.cpp
+++ staticfunction_170/staticfunction_170.cpp
@@ -7,4 +7,16 @@ public:
 	static int nim;
 	int id;
 	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int mahasiswa :: nim = 0;
+
+void mahasiswa :: setID() {
+	id = ++nim;
+}
+
+void mahasiswa::print
